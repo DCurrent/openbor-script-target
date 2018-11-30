3365c256bd246b4560208c652e1a1de1ca340ce0
Caskey, Damon V <dc@caskeys.com>
2018-11-30 17:15:00 -0500

    Add base access and mutate functions.

--- offset.c
+++ offset.c
@@ -2,6 +2,52 @@
 
 #import "data/scripts/dc_target/instance.c"
 
+// Base offset.
+int dc_target_get_offset_base()
+{
+	int result;
+	char id;
+
+	// Concatenate instance with var key
+	// to get a finished variable id.
+	id = dc_target_get_instance() + DC_TARGET_VAR_KEY_OFFSET_BASE;
+
+	// Get current value.
+	result = getlocalvar(id);
+
+	// If the value is blank or wrong type,
+	// use default instead.
+	if (typeof(result) != openborconstant("VT_INTEGER"))
+	{
+		result = DC_TARGET_DEFAULT_OFFSET_BASE;
+	}
+
+	// Return value.
+	return result;
+}
+
+void dc_target_set_offset_base(int value)
+{
+	char id;
+
+	// Concatenate instance with var key
+	// to get a finished variable id.
+	id = dc_target_get_instance() + DC_TARGET_VAR_KEY_OFFSET_BASE;
+
+	// If the value we intend to set is
+	// same as default, then just make sure
+	// the variable is deleted instead. No
+	// reason to waste memory on default values.
+	if (value == DC_TARGET_DEFAULT_OFFSET_BASE)
+	{
+		value = NULL();
+	}
+
+	// Apply value to a local var with
+	// our concatenated id.
+	setlocalvar(id, value);
+}
+
 // X axis offset.
 int dc_target_get_offset_x()
 {
