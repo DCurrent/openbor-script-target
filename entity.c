a465b055dc0dbfd7282201bb885d71e03073b371
Caskey, Damon V <dc@caskeys.com>
2018-11-30 10:10:08 -0500

    Add entity access and mutate.

---
+++ entity.c
@@ -0,0 +1,39 @@
+#include "data/scripts/dc_target/config.h"
+
+#import "data/scripts/dc_target/instance.c"
+
+// Acting entity.
+void dc_target_get_entity()
+{
+	int instance;
+	void result;
+
+	// Get instance.
+	instance = dc_target_get_instance();
+
+	result = getlocalvar(instance + DC_TARGET_VAR_KEY_ENT);
+
+	if (typeof(result) != openborconstant("VT_PTR"))
+	{
+		result = DC_TARGET_DEFAULT_ENT;
+	}
+
+	return result;
+}
+
+void dc_target_set_entity(void value)
+{
+	int instance;
+	void result;
+
+	// Get instance.
+	instance = dc_target_get_instance();
+
+	// Only set the value if new value
+	// is different from default. No reason 
+	// to waste the memory on a default value. 
+	if (value != DC_TARGET_DEFAULT_ENT)
+	{
+		setlocalvar(instance + DC_TARGET_VAR_KEY_ENT, value);
+	}	
+}
--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "data/scripts/dc_target/config.h"
 
 #import "data/scripts/dc_target/instance.c"
+#import "data/scripts/dc_target/entity.c"
 #import "data/scripts/dc_target/offset.c"
