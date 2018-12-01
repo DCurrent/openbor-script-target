00950f1f8747b8ea4c90b2f56d188c3bb2c54238
Caskey, Damon V <dc@caskeys.com>
2018-12-01 18:01:33 -0500

    dc_target_check_range_target_x() uses dc_target_check_range_target_x().

--- range.c
+++ range.c
@@ -46,15 +46,7 @@ float dc_target_find_total_distance_to_target(void target)
 // check_range_target_x() function.
 int dc_target_check_range_target_x(void target)
 {
-	void ent;
-	int ent_pos;
 	int target_pos;
-	int range_min;
-	int range_max;
-	int animation;
-
-	// Acting entity.
-	ent = dc_target_get_entity();
 
 	// Must have a target.
 	if (typeof(target) != openborconstant("VT_PTR"))
@@ -63,41 +55,12 @@ int dc_target_check_range_target_x(void target)
 		return 0;
 	}
 
-	// Get animation and verify entity has it.
-	animation = dc_target_get_animation();
-
-	if (!getentityproperty(ent, "animvalid", animation))
-	{
-		return 0;
-	}
-
 	// Get positions.
-	ent_pos = getentityproperty(ent, "x");
 	target_pos = getentityproperty(target, "x");
 
-	// Return true if final target location is
-	// within range X min and max. Range comparison
-	// is reversed when entity faces left.
-	if (getentityproperty(ent, "direction") == openborconstant("DIRECTION_RIGHT"))
-	{
-		// Add animation range to entity X position
-		// for final X range coordinates.
-		range_min = ent_pos + getentityproperty(ent, "range", "xmin", animation);
-		range_max = ent_pos + getentityproperty(ent, "range", "xmin", animation);
-
-		return (target_pos >= range_min
-			&& target_pos <= range_max);
-	}
-	else
-	{
-		// Subtract animation range from entity X
-		// position for final X range coordinates.
-		range_min = ent_pos - getentityproperty(ent, "range", "xmin", animation);
-		range_max = ent_pos - getentityproperty(ent, "range", "xmin", animation);
-
-		return (target_pos <= range_min
-			&& target_pos >= range_max);
-	}
+	//  Run the range function using target's
+	// position as target position.
+	return dc_target_check_range_target_x(target_pos);
 }
 
 // Caskey, Damon V.
@@ -269,18 +232,17 @@ int dc_target_check_position_in_range_base()
 //
 // Return true if target position is 
 // within animation's X range.
-int dc_target_check_position_in_range_x()
+int dc_target_check_position_in_range_x(float target_pos)
 {
 	void ent;			// Acting entity.
 	int range_min;		// Minimum range.
 	int range_max;		// Maximum range.
-	float pos_current;	// Current entity position.
-	int pos_target;		// Position to varify is in range.
+	float ent_pos;	// Current entity position.
 	int animation;		// Animation to use.
 
 	// Get action ent and position.
 	ent = dc_target_get_entity();
-	pos_current = getentityproperty(ent, "x");
+	ent_pos = getentityproperty(ent, "x");
 
 	// Get animation and verify entity has it.
 	animation = dc_target_get_animation();
@@ -290,32 +252,36 @@ int dc_target_check_position_in_range_x()
 		return 0;
 	}
 
-	// Get target position.
-	pos_target = dc_target_get_offset_x();
-
-	// Get ranges. We're doing the range check
-	// manually, so our range needs to combine
-	// range settings from an animation with
-	// the acting entity's current position.
-	range_min = pos_current + getentityproperty(ent, "range", "xmin", animation);
-	range_max = pos_current + getentityproperty(ent, "range", "xmax", animation);
-
-	// If the target position is less than 
-	// the minimum or greater than the maximum,
-	// return false immediately.
-	if (pos_target < range_min)
+	// Use library offset if no target position provided.
+	if (typeof(target_pos) != openborconstant("VT_DECIMAL"))
 	{
-		return 0;
-	}
-
-	if (pos_target > range_max)
+		// Get target position.
+		target_pos = dc_target_get_offset_x();
+	}	
+	
+	// Return true if final target location is
+	// within range X min and max. Range comparison
+	// is reversed when entity faces left.
+	if (getentityproperty(ent, "direction") == openborconstant("DIRECTION_RIGHT"))
 	{
-		return 0;
-	}
+		// Add animation range to entity X position
+		// for final X range coordinates.
+		range_min = ent_pos + getentityproperty(ent, "range", "xmin", animation);
+		range_max = ent_pos + getentityproperty(ent, "range", "xmin", animation);
 
-	// If we passed all checks, then
-	// we can resturn a true result.
-	return 1;
+		return (target_pos >= range_min
+			&& target_pos <= range_max);
+	}
+	else
+	{
+		// Subtract animation range from entity X
+		// position for final X range coordinates.
+		range_min = ent_pos - getentityproperty(ent, "range", "xmin", animation);
+		range_max = ent_pos - getentityproperty(ent, "range", "xmin", animation);
+
+		return (target_pos <= range_min
+			&& target_pos >= range_max);
+	}
 }
 
 // Caskey, Damon V.
