bd6b4e1ad3a17d4b05cf18d7ec27a35aee6f5c65
Caskey, Damon V <dc@caskeys.com>
2018-12-01 09:12:57 -0500

    Function dc_target_find_obstacle() now finds the nearest item instead of first valid.

--- terrain.c
+++ terrain.c
@@ -5,16 +5,20 @@
 #import "data/scripts/dc_target/offset.c"
 #import "data/scripts/dc_target/range.c"
 
-// Returns x position of first obstacle in range of animation.
+// Caskey, Damon V
+// 2018-12-01 (Retool of original 2017-03-xx)
+//
+// Find nearest obstacle in range of animation.
 int dc_target_find_obstacle() 
 {
-
 	int     result;			// Final result.
 	void	ent;			// Acting entity.
 	int		animation;		// Acting animation.
 	void    target;			// Target entity pointer.
 	int     target_count;	// Target Entity count.
 	int     i;				// Loop cursor.
+	float	distance_last;	// Last nearest distance in loop.
+	float	distance_new;	// Current distance in loop.
 
 	// Get acting entity and animation.
 	ent = dc_target_get_entity();
@@ -44,7 +48,7 @@ int dc_target_find_obstacle()
 			continue;
 		}
 
-		// If the target falls out off any range, 
+		// If the target falls out of any range, 
 		// it's not a valid target, so get out of 
 		// this loop iteration.
 
@@ -63,11 +67,20 @@ int dc_target_find_obstacle()
 			continue;
 		}
 
-		// If we'e made it this far, we've found a valid
-		// target. Use current target as result and
-		// exit loop.
-		result = target;
-		break;
+		// Now we get distance to the current target in loop iteration. 
+		// Then we compare that to distance from previous iteration. If
+		// the previous distance doesn't exisit (this is first iteration)
+		// or this iteration's distance is closer, then we record
+		// distance for next iteration and the target as a result. When 
+		// all targets are iterated, result will be the closest one.
+
+		distance_new = dc_target_find_total_distance_to_target(target);
+
+		if (distance_last || distance_new < distance_last)
+		{
+			distance_last = distance_new;
+			result = target;
+		}
 	}
 
 	// Return final result.
